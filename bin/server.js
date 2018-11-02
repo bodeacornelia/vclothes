6f488b0278073206a475ff982c71ba7df3e198ef
cornelia bodea <cornelia.bodea@evozon.com>
2018-11-02 16:34:07 +0200

    added cors

--- bin/server.js
+++ bin/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 app = express();
+var cors = require('cors');
 bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 dotenv.config();
 port = process.env.PORT 
 const modules = require('../modules')
+
+app.use(cors({
+  origin: 'http://localhost:8080'
+}));
 
 app.listen(port);
 console.log('VClothes RESTful API server started on: ' + port);
